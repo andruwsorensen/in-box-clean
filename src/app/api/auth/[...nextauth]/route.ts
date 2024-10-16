00c3e9a20b102c3e2e8f43d659d1d4145e1065fe
andruwsorensen <alpine.gingy@gmail.com>
2024-10-16 09:50:49 -0600

    Went back to the original

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,2 @@
-import GoogleProvider from "next-auth/providers/google"
-import NextAuth from "next-auth"
-
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      authorization: {
-        params: {
-          prompt: 'consent',
-          access_type: 'offline',
-          response_type: 'code',
-          scope: [
-            'https://www.googleapis.com/auth/gmail.readonly',
-            'https://www.googleapis.com/auth/gmail.settings.basic',
-            'https://www.googleapis.com/auth/gmail.settings.sharing',
-            'https://www.googleapis.com/auth/gmail.modify',
-            'https://mail.google.com/'
-          ]
-        }
-      }
-    }),
-  ],
-})
-
-export { handler as GET, handler as POST }
+import { handlers } from "@/auth" // Referring to the auth.ts we just created
+export const { GET, POST } = handlers
