ca28f6669a33a166dcd695dd7a554acf1c2ac15a
andruwsorensen <alpine.gingy@gmail.com>
2024-10-16 14:54:27 -0600

    asasd

--- src/auth.ts
+++ src/auth.ts
@@ -33,18 +33,4 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       }
     })
   ],
-  callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.access_token = account.access_token
-      }
-      return token
-    },
-    async session({ session, token }) {
-      // Send properties to the client, like an access_token from a provider.
-      session.access_token = token.access_token
-      return session
-    }
-  }
 })
