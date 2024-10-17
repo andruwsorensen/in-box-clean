a9343296439bd13406baffe9a4a320b2d79a12f2
andruwsorensen <alpine.gingy@gmail.com>
2024-10-17 17:43:20 -0600

    Added new changes

--- src/auth.ts
+++ src/auth.ts
@@ -33,4 +33,24 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       }
     })
   ],
+  callbacks: {
+    jwt({token, user}) {
+      if (user) {
+        token.access_token = user.access_token
+        token.refresh_token = user.refresh_token
+        token.scope = user.scope
+        token.token_type = user.token_type
+        token.expiry_date = user.expires_at ? user.expires_at * 1000 : 0
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.access_token = token.access_token as string
+      session.refresh_token = token.refresh_token as string
+      session.scope = token.scope as string
+      session.token_type = token.token_type as string
+      session.expiry_date = token.expiry_date as number
+      return session
+    },
+  }
 })
