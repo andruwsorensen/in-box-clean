ad412d1c3917da1b181c6f60fe3ff2c9069893cb
andruwsorensen <alpine.gingy@gmail.com>
2025-06-07 14:37:52 -0600

    Removing check for pkce temp

--- src/auth.ts
+++ src/auth.ts
@@ -19,6 +19,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Google({
       clientId : process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      checks: ['state'],
       authorization: {
         params: {
           prompt: 'consent',
